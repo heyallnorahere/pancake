77addb2bf641f7fdbe226cc992d4e86fd978a78a
Nora Beda <theo.github@bedafamily.com>
2025-06-09 13:48:10 -0700

    python dependency

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,13 @@ CMAKE_ARGS="-DSDL_VIDEO=OFF"
 
 if [[ "$TARGETARCH" != "$BUILDARCH" ]]; then
     apt-get install -y gcc-$COMPILERARCH-linux-gnu g++-$COMPILERARCH-linux-gnu binutils-$COMPILERARCH-linux-gnu
+
+    dpkg --add-architecture arm64
+    cp -rf $SCRIPTDIR/sources.list.d /etc/apt/
+
+    apt-get update
+    apt-get install -y python3-dev:arm64 python3-numpy:arm64
+
     CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$SCRIPTDIR/$TARGETARCH.cmake"
 fi
 
